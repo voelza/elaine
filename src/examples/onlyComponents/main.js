e328c252e964dcbb4b30ac2d5be8eac3704d42a9
Achim Völz <achim.voelz.1991@gmail.com>
2022-04-06 14:22:09 +0200

    improved image slideshow example

--- src/examples/onlyComponents/main.js
+++ src/examples/onlyComponents/main.js
@@ -114,7 +114,10 @@ const TheCarousel = ELAINE.component({
                     ++click="setSelected(@@img, @@_index)"
                     />
             </div>
-            <img @@src="@@selectedImage.src" @@alt="@@selectedImage.alt"  class="display-image" />
+            <div class="display-image-container">
+                <img @@src="@@image1.src" @@alt="@@image1.alt"  class="display-image" @@style="opacity: @@opacity1##;" />
+                <img @@src="@@image2.src" @@alt="@@image2.alt"  class="display-image" @@style="opacity: @@opacity2##;"/>
+            </div>
             <p>@@{selectedImage.caption}</p>
         </div>
         <div ++click="nextImage" class="btn next-btn">&rang;</div>
@@ -137,6 +140,13 @@ const TheCarousel = ELAINE.component({
             flex-wrap: wrap;
         }
 
+        .image-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 10px;
+        }
+
         .preview-image {
             width: 50px;
             height: 50px;
@@ -151,12 +161,21 @@ const TheCarousel = ELAINE.component({
             box-shadow: 5px 5px 5px lightgray;
         }
 
+        .display-image-container {
+            position:relative;
+            width: 810px;
+            height: 480px;
+        }
+
         .display-image {
+            position:absolute;
+            left:0;
             width: 800px;
             max-width: 85vw;
             border: 3px solid white;
             border-radius: 2.5px;
             box-shadow: 5px 5px 5px lightgray;
+            transition: opacity 1s ease-in-out;
         }
 
         .btn {
@@ -175,6 +194,7 @@ const TheCarousel = ELAINE.component({
         p {
             text-align: center;
             font-family: monospaced;
+            margin-top: -20px;
         }
     `,
     props: [
@@ -184,28 +204,72 @@ const TheCarousel = ELAINE.component({
         }
     ],
     setup: (state) => {
+        const getPreviousIndex = (index) => {
+            let i = index;
+            i--;
+            if (i < 0) {
+                i = state.data.images.value.length - 1;
+            }
+            return i;
+        }
+
+        const getNextIndex = (index) => {
+            let i = index;
+            i++;
+            if (i === state.data.images.value.length) {
+                i = 0;
+            }
+            return i;
+        }
+
         let currentIndex = 0;
         const selectedImage = ELAINE.state(state.data.images.value[currentIndex]);
 
-        const prevImage = () => {
-            currentIndex--;
-            if (currentIndex < 0) {
-                currentIndex = state.data.images.value.length - 1;
+        const image1 = ELAINE.state(selectedImage.value);
+        const opacity1 = ELAINE.state(0);
+        const image2 = ELAINE.state(state.data.images.value[getNextIndex(currentIndex)]);
+        const opacity2 = ELAINE.state(0);
+
+        const swapImages = (imgFromBefore) => {
+            if (image1.value === imgFromBefore) {
+                image2.value = selectedImage.value;
+                opacity1.value = 0;
+                opacity2.value = 1;
+            } else {
+                image1.value = selectedImage.value;
+                opacity1.value = 1;
+                opacity2.value = 0;
             }
+        }
+
+
+        swapImages(selectedImage.value);
+
+        const prevImage = () => {
+            const imgFromBefore = selectedImage.value;
+
+            currentIndex = getPreviousIndex(currentIndex);
             selectedImage.value = state.data.images.value[currentIndex];
+
+            swapImages(imgFromBefore);
         };
 
         const nextImage = () => {
-            currentIndex++;
-            if (currentIndex === state.data.images.value.length) {
-                currentIndex = 0;
-            }
+            const imgFromBefore = selectedImage.value;
+
+            currentIndex = getNextIndex(currentIndex);
             selectedImage.value = state.data.images.value[currentIndex];
+
+            swapImages(imgFromBefore);
         };
 
         const setSelected = (image, index) => {
+            const imgFromBefore = selectedImage.value;
+
             currentIndex = index;
             selectedImage.value = image;
+
+            swapImages(imgFromBefore);
         }
 
         const interval = setInterval(nextImage, 5000);
@@ -214,8 +278,11 @@ const TheCarousel = ELAINE.component({
                 selectedImage,
                 prevImage,
                 nextImage,
-                interval,
-                setSelected
+                setSelected,
+                image1,
+                image2,
+                opacity1,
+                opacity2
             }
         }
     },
